094b0ca18b50f0555d6870e302c90735c04c96ae
virtuozrus <virtuoz_@bk.ru>
2019-01-29 18:27:46 +0700

    fix breadcrumb

--- resources/views/admin/components/breadcrump.blade.php
+++ resources/views/admin/components/breadcrump.blade.php
@@ -1,5 +1,5 @@
 <h2>{{$title}}</h2>
 <ol class="breadcrumb">
-    <li><a href="{{route('admin.index')}}">{{$parent}}</a></li>
-    <li class="active">{{$active}}</li>
+    <li class="breadcrumb-item"><a href="{{route('admin.index')}}">{{$parent}}</a></li>
+    <li class="breadcrumb-item active" aria-current="page">{{$active}}</li>
 </ol>
